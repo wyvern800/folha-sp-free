b647ae4a344d62724896db627b3a5b5329235a37
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2023-07-22 22:54:13 -0300

    feat: código mais bonito

--- content.js
+++ content.js
@@ -1,72 +1,31 @@
-// Your function to manipulate components
 let removed = [];
 
-function manipulateComponents() {
-  let hasAllComponentsRemoved =
-    removed?.length === 4 &&
-    removed.includes("paywallFill") &&
-    removed.includes("paywallFlutuante") &&
-    removed.includes("cTopSignup") &&
-    removed.includes("paywallContent");
+const componentsToRemove = [
+  { name: "paywallFill", elementId: "paywall-fill" },
+  { name: "paywallFlutuante", elementId: "paywall-flutuante" },
+  { name: "cTopSignup", elementId: "top-signup-close-bf" },
+  { name: "paywallContent", elementId: "paywall-content" },
+];
 
-  if (hasAllComponentsRemoved) { 
+function manipulateComponents() {
+  if (componentsToRemove.every(({ name }) => removed.includes(name))) {
     console.log('skipped');
-    
-    return; 
+    return;
   }
 
-  const paywallFill = document.getElementById("paywall-fill");
-  const paywallFlutuante = document.getElementById("paywall-flutuante");
-  const cTopSignup = document.getElementById("top-signup-close-bf");
-  const paywallContent = document.getElementById("paywall-content");
-
-  const actions = [
-    {
-      name: "paywallFill",
-      exec: () => {
-        if (paywallFill && !removed.includes("paywallFill")) {
-          paywallFill.style.backgroundImage = "";
-          paywallFill.style.opacity = 0;
-          removed.push("paywallFill");
-        }
-      },
-    },
-    {
-      name: "paywallFlutuante",
-      exec: () => {
-        if (paywallFlutuante && !removed.includes("paywallFlutuante")) {
-          paywallFlutuante.remove();
-          removed.push("paywallFlutuante");
-        }
-      },
-    },
-    {
-      name: "cTopSignup",
-      exec: () => {
-        if (cTopSignup && !removed.includes("cTopSignup")) {
-          cTopSignup.remove();
-          removed.push("cTopSignup");
-        }
-      },
-    },
-    {
-      name: "paywallContent",
-      exec: () => {
-        if (paywallContent && !removed.includes("paywallContent")) {
-          paywallContent.style.overflowY = "scroll";
-          removed.push("paywallContent");
-        }
-      },
-    },
-  ];
-
-  for (const action of actions) {
-    console.log(action.name + " executed!");
-    action.exec();
+  for (const { name, elementId } of componentsToRemove) {
+    const component = document.getElementById(elementId);
+    if (component && !removed.includes(name)) {
+      if (name === "paywallContent") {
+        component.style.overflowY = "scroll";
+      } else {
+        component.remove();
+      }
+      removed.push(name);
+      console.log(name + " executed!");
+    }
   }
 }
 
-// Set the interval to run your function every 2 seconds (adjust as needed)
 const intervalTime = 2000; // 2 seconds
-
 setInterval(manipulateComponents, intervalTime);
