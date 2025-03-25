1ce7a69d78f2454db845ba89419d0918c233d96e
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2025-03-25 20:12:31 -0300

    fix: remove the subscribe call elements

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 const originalFetch = window.fetch;
+
 window.fetch = function (url, options) {
   if (url === 'https://paywall.folha.uol.com.br/wall.json') {
     console.log('Blocking request:', url);
@@ -7,3 +8,33 @@ window.fetch = function (url, options) {
   }
   return originalFetch(url, options);  
 };
+
+let removed = [];
+
+const componentsToRemove = [
+  { name: "c-top-signup-inner", elementId: "top-signup-close" },
+  { name: "c-top-signup--secondary", className: "c-top-signup--secondary" },
+  { name: "c-top-signup", className: "c-top-signup" },
+  { name: "c-subscribe-ads__description", className: "c-subscribe-ads__description" },
+  { name: "comments-tooltip--blue", className: "comments-tooltip--blue" },
+  { name: "c-subscribe-ads", className: "c-subscribe-ads" },
+];
+
+function manipulateComponents() {
+  if (componentsToRemove.every(({ name }) => removed.includes(name))) {
+    console.log('skipped');
+    return;
+  }
+
+  for (const { name, elementId } of componentsToRemove) {
+    const component = document.getElementById(elementId) || document.getElementsByClassName(name)[0];
+    if (component && !removed.includes(name)) {
+      component.remove();
+      removed.push(name);
+      console.log(name + " executed!");
+    }
+  }
+}
+
+const intervalTime = 2000; // 2 seconds
+setInterval(manipulateComponents, intervalTime);
